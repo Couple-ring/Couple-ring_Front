af6162858bbfc51581ce0bd41f709f39e23e6d5c
안윤지 <dksdbswl10430@naver.com>
2022-10-26 12:48:43 +0900

    Setting✨ 라우트 세팅

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import LoginPage from './page/LoginPage';
+import SignupPage from './page/SignupPage';
+import Header from './components/Header';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   )
