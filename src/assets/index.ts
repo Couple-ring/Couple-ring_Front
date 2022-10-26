34c2b03575f751f4051a174924d938273c60feee
안윤지 <dksdbswl10430@naver.com>
2022-10-26 12:45:59 +0900

    Add🌻이미지 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,4 @@ export { default as CoupleringLogo } from "./imgs/logo.svg";
 export { default as Profile } from "./imgs/person.svg";
 export { default as List } from "./imgs/list.svg";
 export { default as Door } from "./imgs/door.svg";
+export { default as ClickProfile } from "./imgs/clickProfile.svg";
