5d8b7c2217b7c75f03a4c6f044b8527918e9195c
안윤지 <dksdbswl10430@naver.com>
2022-10-27 23:32:02 +0900

    Add🌻이미지 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,3 +3,5 @@ export { default as Profile } from "./imgs/person.svg";
 export { default as List } from "./imgs/list.svg";
 export { default as Door } from "./imgs/door.svg";
 export { default as ClickProfile } from "./imgs/clickProfile.svg";
+export { default as ReviseIcon } from "./imgs/reviseIcon.svg";
+export { default as Heart } from "./imgs/heart.svg";
