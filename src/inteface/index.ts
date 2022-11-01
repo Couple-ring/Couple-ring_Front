fc887ba90644b8eb9ef6a3ab26d55bddf3755eb6
안윤지 <dksdbswl10430@naver.com>
2022-11-01 16:38:36 +0900

    Add🌻인터페이스 추가

--- src/inteface/index.ts
+++ src/inteface/index.ts
@@ -1,4 +1,13 @@
 export interface AuthType {
   title: string;
   type: string;
-}
+};
+
+export interface DiaryType {
+  name: string;
+  mood: string;
+  title: string;
+  content: string;
+  img: string;
+  mine: boolean;
+};
