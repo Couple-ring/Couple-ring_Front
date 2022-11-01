db920a115425f424d88975d58ca8188332f74f1e
안윤지 <dksdbswl10430@naver.com>
2022-11-01 16:36:38 +0900

    Setting✨ 라우터 설정

--- src/router.tsx
+++ src/router.tsx
@@ -2,14 +2,17 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import LoginPage from './page/LoginPage';
 import SignupPage from './page/SignupPage';
 import Header from './components/Header';
+import MainPage from './page/MainPage';
 
 function Router() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path='/*' element={<LoginPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
+        <Route path='/main' element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   )
