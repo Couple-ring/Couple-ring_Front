e7235043aa7411d53253afc0af8e6a56a35b1e53
안윤지 <dksdbswl10430@naver.com>
2022-11-01 16:35:47 +0900

    Add🌻색상 추가

--- src/style/index.ts
+++ src/style/index.ts
@@ -3,7 +3,8 @@ export const PointColor = "#FFBDDD";
 
 export const COLOR = {
   red: "#FF8181",
-  pink:"#FFD1D1",
+  pink: "#FFD1D1",
+  black: "#423B3B",
 };
 
 export const Font = "'Noto Sans KR', sans-serif;";
