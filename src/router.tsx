191d86a3cedced29383353cc98b9fb4144b9a8b7
안윤지 <dksdbswl10430@naver.com>
2022-11-14 16:15:49 +0900

    Setting✨ 라우터 설정

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import SignupPage from './page/SignupPage';
 import Header from './components/Header';
 import MainPage from './page/MainPage';
 import WatchingDiaryPage from './page/WatchingDiaryPage';
+import ReviseDiaryPage from './page/ReviseDiaryPage';
 
 function Router() {
   return (
@@ -15,6 +16,7 @@ function Router() {
         <Route path='/signup' element={<SignupPage />} />
         <Route path='/main' element={<MainPage />} />
         <Route path='/watch_diary' element={<WatchingDiaryPage />} />
+        <Route path='/revise_diary' element={<ReviseDiaryPage />} />
       </Routes>
     </BrowserRouter>
   )
