780420303b5a99c428f40a63dbd53cd63e8e3dbc
안윤지 <dksdbswl10430@naver.com>
2022-12-02 15:34:50 +0900

    Add🌻 다이어리 수정 기능

--- src/components/ReviseDiary/index.tsx
+++ src/components/ReviseDiary/index.tsx
@@ -11,23 +11,30 @@ import {
   Soso,
   Angry,
 } from "../../assets";
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { getAccessToken } from "../../utils/Token";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const ReviseDiary = () => {
-  const fileInput = useRef<any>(null);
-  // 사용하지 않는 코드는 제거 필요1
+  const navigate = useNavigate();
+  // const fileInput = useRef<any>(null);
   const { id } = useParams();
   const [isMood, setIsMood] = useState<boolean>(false);
-  // 임시로 state 작성해두었고, 해당 state name & 사용 방법 수정 필요
-  const [a, setA] = useState<any>(Test);
-  const [myDiary, setMyDiary] = useState<any>([]);
-  const [mood, setMood] = useState<string>(Soso);
+  const [fileName, setFileName] = useState<any>(Test);
+  const [moodImg, setMoodImg] = useState<string>(Happy);
   const [preview, setPreview] = useState(Test);
+  const [resImg, setResImg] = useState();
+
+  const [reviseDiary, setReviseDiary] = useState({
+    title: "",
+    content: "",
+  });
+  const { title, content } = reviseDiary;
+
   const today: string =
     new Date().getFullYear() +
     "-" +
@@ -35,47 +42,67 @@ const ReviseDiary = () => {
     "-" +
     new Date().getDate();
 
-  const [reviseDiary, setReviseDiary] = useState({
-    title: "",
-    content: "",
-  });
+  const settingMood = () => {
+    switch (moodImg) {
+      case Happy:
+        return "Happy";
+      case Sad:
+        return "Sad";
+      case Soso:
+        return "Soso";
+      case Angry:
+        return "Angry";
+    }
+  };
 
-  // 사용하지 않는 코드는 제거 필요2
-  const { title, content } = reviseDiary;
+  const mood = settingMood();
 
-  const getMyDiary = async () => {
+  // const upLoadFile = async () => {
+  //   const access_token = getAccessToken();
+  //   const formData = new FormData();
+  //   let file = new Blob([preview]);
+  //   formData.append("file", file, fileName);
+  //   await axios
+  //     .post(`${BASE_URL}/file`, formData, {
+  //       headers: {
+  //         "Content-Type": "multipart/form-data",
+  //         Authorization: `Bearer ${access_token}`,
+  //       },
+  //     })
+  //     .then((res) => {
+  //       setResImg(res.data.fileName);
+  //     });
+  // };
+
+  const revise = async () => {
     const access_token = getAccessToken();
     await axios
-      .get(`${BASE_URL}/diaries?date=${today}`, {
-        headers: { Authorization: `Bearer ${access_token}` },
-      })
-      .then((res) => {
-        setMyDiary(res.data.myDiary);
+      .patch(
+        `${BASE_URL}/diaries/${id}`,
+        {
+          title: title,
+          mood: mood,
+          fileId: resImg,
+          content: content,
+        },
+        { headers: { Authorization: `Bearer ${access_token}` } }
+      )
+      .then(() => {
+        alert("오늘자 다이어리가 수정되었습니다.");
+        navigate("/main");
       });
   };
 
-  useEffect(() => {
-    getMyDiary();
-  }, []);
-
-  const upLoadFile = async () => {
-    const access_token = getAccessToken();
-    const formData = new FormData();
-    let file = new Blob([preview]);
-    formData.append("file", file, a);
-    await axios.post(`${BASE_URL}/file`, formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: `Bearer ${access_token}`,
-      },
-    });
-  };
-
   const onRevise = async () => {
-    await upLoadFile();
+    try {
+      await revise();
+    } catch (error) {
+      console.log(error);
+    }
+    // await upLoadFile();
   };
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onChange = (e: any) => {
     const { name, value } = e.target;
     setReviseDiary({
       ...reviseDiary,
@@ -83,38 +110,34 @@ const ReviseDiary = () => {
     });
   };
 
-  const imgChange = (e: any) => {
-    if (e.target.files[0]) {
-      setPreview(e.target.files[0]);
-      setA(e.target.files[0].name);
-    } else {
-      setPreview(preview);
-      return;
-    }
-    const reader: any = new FileReader();
-    reader.onload = () => {
-      if (reader.readyState === 2) {
-        setPreview(reader.result);
-      }
-    };
-    reader.readAsDataURL(e.target.files[0]);
-  };
+  // const imgChange = (e: any) => {
+  //   if (e.target.files[0]) {
+  //     setPreview(e.target.files[0]);
+  //     setFileName(e.target.files[0].name);
+  //   } else {
+  //     setPreview(preview);
+  //     return;
+  //   }
+  //   const reader: any = new FileReader();
+  //   reader.onload = () => {
+  //     if (reader.readyState === 2) {
+  //       setPreview(reader.result);
+  //     }
+  //   };
+  //   reader.readAsDataURL(e.target.files[0]);
+  // };
 
   return (
     <>
       <S.Container>
         <S.Head>
-          <img src={mood} />
+          <img src={moodImg} />
           <img onClick={() => setIsMood(!isMood)} src={ReviseIcon} />
         </S.Head>
         <S.Wrapper>
-          <S.Title
-            name="title"
-            onChange={onChange}
-            defaultValue={myDiary.title}
-          />
-          <S.Content name="content" defaultValue={myDiary.content} />
-          <input
+          <S.Title name="title" onChange={onChange} />
+          <S.Content name="content" onChange={onChange} />
+          {/* <input
             onChange={imgChange}
             type="file"
             ref={fileInput}
@@ -125,32 +148,32 @@ const ReviseDiary = () => {
             onClick={() => {
               fileInput.current.click();
             }}
-          />
+          /> */}
         </S.Wrapper>
         <S.ReviseBtn onClick={onRevise}>수정하기</S.ReviseBtn>
       </S.Container>
-      {isMood && <Moods setMood={setMood} />}
+      {isMood && <Moods setMoodImg={setMoodImg} />}
     </>
   );
 };
 
 interface MoodModalProps {
-  setMood: React.Dispatch<React.SetStateAction<string>>;
+  setMoodImg: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const Moods = ({ setMood }: MoodModalProps) => {
+const Moods = ({ setMoodImg }: MoodModalProps) => {
   return (
     <S.Box>
-      <S.ImgBox onClick={() => setMood(Happy)}>
+      <S.ImgBox onClick={() => setMoodImg(Happy)}>
         <img src={HappyPink} />
       </S.ImgBox>
-      <S.ImgBox onClick={() => setMood(Angry)}>
+      <S.ImgBox onClick={() => setMoodImg(Angry)}>
         <img src={AngryPink} />
       </S.ImgBox>
-      <S.ImgBox onClick={() => setMood(Soso)}>
+      <S.ImgBox onClick={() => setMoodImg(Soso)}>
         <img src={SosoPink} />
       </S.ImgBox>
-      <S.ImgBox onClick={() => setMood(Sad)}>
+      <S.ImgBox onClick={() => setMoodImg(Sad)}>
         <img src={SadPink} />
       </S.ImgBox>
     </S.Box>
