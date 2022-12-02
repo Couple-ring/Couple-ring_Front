a43272e71f18c1a8f942f7628dd299b885a2d10f
안윤지 <dksdbswl10430@naver.com>
2022-12-02 15:34:55 +0900

    Add🌻 다이어리 작성 기능

--- src/components/WritingDiary/index.tsx
+++ src/components/WritingDiary/index.tsx
@@ -1,52 +1,124 @@
 import * as S from "./styles";
 import { useState, useRef } from "react";
-import { HappyPink, SadPink, SosoPink, AngryPink, Test, ReviseIcon, Happy, Sad, Soso, Angry } from "../../assets";
+import {
+  HappyPink,
+  SadPink,
+  SosoPink,
+  AngryPink,
+  Test,
+  ReviseIcon,
+  Happy,
+  Sad,
+  Soso,
+  Angry,
+  BlueReviseIcon,
+} from "../../assets";
+import axios from "axios";
+import { getAccessToken } from "../../utils/Token";
+import { useNavigate } from "react-router-dom";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const WritingDiary = () => {
-  const today: string = new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate();
-  const [mood, setMood] = useState<string>(HappyPink);
+  const navigate = useNavigate();
+  const today: string = new Date().toISOString().slice(0, 10);
+  const [moodImg, setMoodImg] = useState<string>(HappyPink);
   const [isMoodModal, setIsMoodModal] = useState<boolean>(false);
   const [preview, setPreview] = useState(Test);
-  const fileInput = useRef<any>(null);
-  const [inputs, setInputs] = useState({
-    title: "",
-    mood: "",
-    content: "",
-  });
+  // const fileInput = useRef<any>(null);
 
-  const imgChange = (e: any) => {
-    if (e.target.files[0]) {
-      setPreview(e.target.files[0])
-    } else {
-      setPreview(preview);
-      return;
+  const settingMood = () => {
+    switch (moodImg) {
+      case HappyPink:
+        return "Happy";
+      case SadPink:
+        return "Sad";
+      case SosoPink:
+        return "Soso";
+      case AngryPink:
+        return "Angry";
     }
-    const reader: any = new FileReader();
-    reader.onload = () => {
-      if (reader.readyState === 2) {
-        setPreview(reader.result);
-      }
-    };
-    reader.readAsDataURL(e.target.files[0]);
   };
 
+  const mood = settingMood();
+
+  const [inputs, setInputs] = useState({
+    title: "",
+    content: "",
+  });
+  const { title, content } = inputs;
+
+  // const imgChange = (e: any) => {
+  //   if (e.target.files[0]) {
+  //     setPreview(e.target.files[0]);
+  //   } else {
+  //     setPreview(preview);
+  //     return;
+  //   }
+  //   const reader: any = new FileReader();
+  //   reader.onload = () => {
+  //     if (reader.readyState === 2) {
+  //       setPreview(reader.result);
+  //     }
+  //   };
+  //   reader.readAsDataURL(e.target.files[0]);
+  // };
+
+  const onChange = (e: any) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const post = async () => {
+    const access_token = getAccessToken();
+    await axios.post(
+      `${BASE_URL}/diaries`,
+      {
+        title: title,
+        mood: mood,
+        fileId: null,
+        content: content,
+      },
+      {
+        headers: { Authorization: `Bearer ${access_token}` },
+      }
+    );
+  };
+
+  const onPost = async () => {
+    try {
+      await post();
+      alert("다이어리가 성공적으로 생성되었습니다.");
+      navigate("/main");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
       <S.Container>
         <S.Head>오늘&#41; {today}</S.Head>
         <S.Inputs>
-          <S.TitleInput placeholder="제목" />
-          <S.MoodInput
-            defaultValue={mood}
-            onClick={() => setIsMoodModal(!isMoodModal)}
-          >
-            <img src={mood} />
+          <S.TitleInput placeholder="제목" name="title" onChange={onChange} />
+          <S.MoodInput defaultValue={moodImg}>
+            <img src={moodImg} />
+            <img
+              src={BlueReviseIcon}
+              onClick={() => setIsMoodModal(!isMoodModal)}
+            />
           </S.MoodInput>
-          <S.DiaryContent placeholder="내용"></S.DiaryContent>
+          <S.DiaryContent
+            placeholder="내용"
+            name="content"
+            onChange={onChange}
+          />
         </S.Inputs>
-        <S.PostBtn>게시하기</S.PostBtn>
-        <input
+        <S.PostBtn onClick={onPost}>게시하기</S.PostBtn>
+        {/* <input
           onChange={imgChange}
           type="file"
           ref={fileInput}
@@ -54,28 +126,37 @@ const WritingDiary = () => {
         />
         <img
           src={preview}
-          onClick={() => { fileInput.current.click() }}
-        />
+          onClick={() => {
+            fileInput.current.click();
+          }}
+        /> */}
       </S.Container>
-      {isMoodModal && <MoodModal setMood={setMood} />}
+      {isMoodModal && <MoodModal setMoodImg={setMoodImg} />}
     </>
   );
 };
 
 interface MoodModalProps {
-  setMood: React.Dispatch<React.SetStateAction<string>>;
-};
+  setMoodImg: React.Dispatch<React.SetStateAction<string>>;
+}
 
-const MoodModal = ({ setMood }: MoodModalProps) => {
+const MoodModal = ({ setMoodImg }: MoodModalProps) => {
   return (
     <S.Box>
-      <S.ImgBox onClick={() => setMood(HappyPink)}><img src={HappyPink} /></S.ImgBox>
-      <S.ImgBox onClick={() => setMood(AngryPink)}><img src={AngryPink} /></S.ImgBox>
-      <S.ImgBox onClick={() => setMood(SosoPink)}><img src={SosoPink} /></S.ImgBox>
-      <S.ImgBox onClick={() => setMood(SadPink)}><img src={SadPink} /></S.ImgBox>
+      <S.ImgBox onClick={() => setMoodImg(HappyPink)}>
+        <img src={HappyPink} />
+      </S.ImgBox>
+      <S.ImgBox onClick={() => setMoodImg(AngryPink)}>
+        <img src={AngryPink} />
+      </S.ImgBox>
+      <S.ImgBox onClick={() => setMoodImg(SosoPink)}>
+        <img src={SosoPink} />
+      </S.ImgBox>
+      <S.ImgBox onClick={() => setMoodImg(SadPink)}>
+        <img src={SadPink} />
+      </S.ImgBox>
     </S.Box>
   );
 };
 
-
-export default WritingDiary; 
+export default WritingDiary;
